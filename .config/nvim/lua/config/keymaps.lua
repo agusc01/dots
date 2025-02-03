072122671a3666372a6cd520c3f12ef8302e19a7
agus <33077305+agusc01@users.noreply.github.com>
2025-02-02 23:22:07 -0300

    More things

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,12 +1,90 @@
 local map = vim.keymap.set
+local del = vim.keymap.del
+
+local function telescope_function(func, desc)
+	local cwd = vim.fn.getcwd()
+	require("telescope.builtin")[func]({
+		cwd = cwd,
+		layout_strategy = "horizontal",
+		layout_config = { prompt_position = "top" },
+		prompt_title = desc ~= "" and desc or nil, -- Set only if desc is not empty
+		sorting_strategy = "ascending",
+		smart_case = true,
+		attach_mappings = function(_, map)
+			local actions_telescope = require("telescope.actions")
+			local actions_fzf = require("fzf-lua.actions")
+
+			-- Teclas para mover la selección
+			map("i", "<C-j>", actions_telescope.move_selection_next)
+			map("i", "<C-k>", actions_telescope.move_selection_previous)
+
+			-- Usar las acciones de fzf-lua
+			-- map("i", "<A-i>", actions_fzf.toggle_ignore) -- alt-i
+			-- map("i", "<A-h>", actions_fzf.toggle_hidden) -- alt-h
+
+			return true
+		end,
+	})
+end
+
+local function telescope_map(modes, func, desc, key)
+	local excluded_funcs = {
+		grep_string = true,
+	}
+	local new_desc = excluded_funcs[func] and "" or desc
+	map(modes, key, function()
+		telescope_function(func, new_desc)
+	end, { desc = desc })
+end
+
+telescope_map({ "n", "v", "i" }, "command_history", "Command History", "<C-g>")
+telescope_map({ "n", "v", "i" }, "live_grep", "Search word", "<C-f>")
+
+telescope_map({ "n", "v", "i" }, "search_history", "Search History", "<C-m>")
+
+telescope_map({ "n", "v", "i" }, "find_files", "Find files", "<C-p>")
+telescope_map({ "n", "v", "i" }, "oldfiles", "Find recent files", "<C-o>")
+telescope_map({ "n", "v", "i" }, "grep_string", "Find current word", "<C-t>")
+
+map("n", "=", function()
+	local word = vim.fn.expand("<cword>")
+	require("telescope.builtin").grep_string({
+		search = word,
+		cwd = vim.fn.getcwd(),
+		layout_config = { prompt_position = "top" },
+		prompt_title = "Search in current file : " .. word,
+		sorting_strategy = "ascending",
+		smart_case = true,
+		attach_mappings = function(_, map)
+			local actions = require("telescope.actions")
+			map("i", "<C-j>", actions.move_selection_next)
+			map("i", "<C-k>", actions.move_selection_previous)
+			return true
+		end,
+	})
+end, { desc = "Search in current file " })
+
+telescope_map({ "n", "v", "i" }, "registers", "History registers", "<C-r>")
+
+map("n", "&", function()
+	telescope_function("current_buffer_fuzzy_find", "Search in current file")
+end, { desc = "Search in current file" })
 
 map("n", "+", "<C-a>", { desc = "Increment number" })
 map("n", "-", "<C-x>", { desc = "Decrement number" })
 map("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
 
+map({ "n", "v" }, "<Leader>t", function()
+	vim.cmd("split | resize 10 | terminal")
+end, { desc = "Open terminal (split)" })
+
+map({ "n", "v" }, "<Leader>T", function()
+	vim.cmd("vsplit | resize 10 | terminal")
+end, { desc = "Open terminal (vsplit)" })
+
 -- map("n", "<Leader>o", "o<Esc>^Da", { desc = "Disable continuations" })
 -- map("n", "<Leader>O", "O<Esc>^Da", { desc = "Disable continuations" })
---
+
 map("n", "te", ":tabedit", { desc = "New tab" })
 map("n", "<C-Tab>", ":tabnext<CR>", { desc = "Next tab" })
 map("n", "<C-S-Tab>", ":tabprev<CR>", { desc = "Previous tab" })
@@ -37,39 +115,6 @@ end, { desc = "Toggle Inlay Hints" })
 
 map({ "v", "i" }, "nn", "<Esc>", { desc = "Escape from visual or insert node" })
 
-local function telescope(modes, func, desc, key)
-	map(modes, key, function()
-		require("telescope.builtin")[func]({
-			cwd = vim.fn.getcwd(),
-			layout_config = { prompt_position = "top" },
-			sorting_strategy = "ascending",
-			smart_case = true,
-			attach_mappings = function(_, map)
-				local actions = require("telescope.actions")
-				map("i", "<C-j>", actions.move_selection_next)
-				map("i", "<C-k>", actions.move_selection_previous)
-				return true
-			end,
-		})
-	end, { desc = desc })
-end
-
-telescope("n", "command_history", "Command History", "<C-p>")
-telescope("n", "search_history", "Search History", "<C-o>")
-telescope("n", "live_grep", "Find words (grep)", "<C-f>")
-telescope("n", "find_files", "Find files (fgrep)", "<C-r>")
-telescope("n", "oldfiles", "Find oldfiles (fgrep)", "<C-t>")
-telescope("n", "grep_string", "Find current word (fgrep)", "<C-g>")
-telescope("n", "registers", "History registers", "<C-m>")
-
-map({ "n", "v" }, "<Leader>t", function()
-	vim.cmd("split | resize 10 | terminal")
-end, { desc = "Open terminal (split)" })
-
-map({ "n", "v" }, "<Leader>T", function()
-	vim.cmd("vsplit | resize 10 | terminal")
-end, { desc = "Open terminal (vsplit)" })
-
 map("n", "<Leader>d", "yyp", { desc = "Duplicate line" })
 map("v", "<Leader>d", "y<Esc>p", { desc = "Duplicate line" })
 
@@ -79,6 +124,7 @@ map("v", "<S-Tab>", "<gv", { desc = "Decrement tabulation" })
 map("n", "<S-Tab>", "<<_", { desc = "Decrement tabulation" })
 
 map("v", "$", "$h", { desc = "Back to a character" })
+map("n", "*", "*N", { desc = "Back in the first search" })
 
 map("v", "<Leader>ñ", "<Plug>(comment_toggle_linewise_visual)", { desc = "Toggle Comment Line" })
 map("n", "<Leader>ñ", "<Plug>(comment_toggle_linewise_current)", { desc = "Toggle Comment Line" })
@@ -92,5 +138,7 @@ map("n", "<leader>K", "v0", { desc = "Select from cursor position and go up (v0)
 
 map({ "n", "v" }, "<Leader>m", "%", { desc = "Jumping (%)" })
 
-map("n", "<C-w>", ":confirm wq<CR>", { desc = "Save file" })
-map({ "v", "i" }, "<C-w>", "<Esc>:confirm wq<CR>", { desc = "Save file" })
+map("n", "<C-w>", ":q<Cr>", { desc = "Save file", silent = false })
+map({ "v", "i" }, "<C-w>", "<Esc>:q<Cr>", { desc = "Save file", silent = false })
+
+del("n", "<Cr>")
