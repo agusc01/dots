7ea90ac7dcb552b0a8bc04a2804c796952233342
agus <33077305+agusc01@users.noreply.github.com>
2025-02-03 00:09:31 -0300

    Basic

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -27,6 +27,43 @@ local function telescope_function(func, desc)
 	})
 end
 
+local function fzf_function(func, desc)
+	local cwd = vim.fn.getcwd()
+	require("fzf-lua").files()
+	require("fzf-lua")[func]({
+		cwd = cwd,
+		layout_strategy = "horizontal",
+		layout_config = { prompt_position = "top" },
+		prompt_title = desc ~= "" and desc or nil, -- Set only if desc is not empty
+		sorting_strategy = "ascending",
+		smart_case = true,
+		attach_mappings = function(_, map)
+			local actions_telescope = require("telescope.actions")
+			local actions_fzf = require("fzf-lua.actions")
+
+			-- Teclas para mover la selección
+			map("i", "<C-j>", actions_telescope.move_selection_next)
+			map("i", "<C-k>", actions_telescope.move_selection_previous)
+
+			-- Usar las acciones de fzf-lua
+			-- map("i", "<A-i>", actions_fzf.toggle_ignore) -- alt-i
+			-- map("i", "<A-h>", actions_fzf.toggle_hidden) -- alt-h
+
+			return true
+		end,
+	})
+end
+
+local function fzf_map(modes, func, desc, key)
+	local excluded_funcs = {
+		grep_string = true,
+	}
+	local new_desc = excluded_funcs[func] and "" or desc
+	map(modes, key, function()
+		fzf_function(func, new_desc)
+	end, { desc = desc })
+end
+
 local function telescope_map(modes, func, desc, key)
 	local excluded_funcs = {
 		grep_string = true,
@@ -37,12 +74,20 @@ local function telescope_map(modes, func, desc, key)
 	end, { desc = desc })
 end
 
+map({ "n", "v", "i" }, "<C-p>", function()
+	require("fzf-lua").files({ root = false })
+end, { desc = "fzf files" })
+
+map({ "n", "v", "i" }, "<C-p>", function()
+	require("fzf-lua").live_grep({ root = false })
+end, { desc = "fzf files" })
+
 telescope_map({ "n", "v", "i" }, "command_history", "Command History", "<C-g>")
-telescope_map({ "n", "v", "i" }, "live_grep", "Search word", "<C-f>")
+-- telescope_map({ "n", "v", "i" }, "live_grep", "Search word", "<C-f>")
 
 telescope_map({ "n", "v", "i" }, "search_history", "Search History", "<C-m>")
 
-telescope_map({ "n", "v", "i" }, "find_files", "Find files", "<C-p>")
+-- telescope_map({ "n", "v", "i" }, "find_files", "Find files", "<C-p>")
 telescope_map({ "n", "v", "i" }, "oldfiles", "Find recent files", "<C-o>")
 telescope_map({ "n", "v", "i" }, "grep_string", "Find current word", "<C-t>")
 
@@ -105,14 +150,6 @@ map("n", "<S-j>", "<C-w>-", { desc = "Resize window v+" })
 -- map("n", "<C-j>", function()
 -- 	vim.diagnostic.goto_next()
 -- end, tt)
-
-map("n", "<Leader>r", function()
-	require("craftzdog.hsl").replaceHexWithHSL()
-end)
-map("n", "<Leader>p", function()
-	require("craftzdog.lsp").toggleInlayHints()
-end, { desc = "Toggle Inlay Hints" })
-
 map({ "v", "i" }, "nn", "<Esc>", { desc = "Escape from visual or insert node" })
 
 map("n", "<Leader>d", "yyp", { desc = "Duplicate line" })
@@ -141,4 +178,4 @@ map({ "n", "v" }, "<Leader>m", "%", { desc = "Jumping (%)" })
 map("n", "<C-w>", ":q<Cr>", { desc = "Save file", silent = false })
 map({ "v", "i" }, "<C-w>", "<Esc>:q<Cr>", { desc = "Save file", silent = false })
 
-del("n", "<Cr>")
+del({ "n", "v", "i" }, "<Cr>")
